715799e9d18abde4c71794fd8f06776299a83625
Clement Escoffier <clement.escoffier@gmail.com>
2013-10-29 19:51:09 +0100

    Temporary solution to load assets from bundles
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

--- resource-controller/src/main/java/org/ow2/chameleon/wisdom/controller/resources/ResourceController.java
+++ resource-controller/src/main/java/org/ow2/chameleon/wisdom/controller/resources/ResourceController.java
@@ -5,6 +5,8 @@ import org.apache.felix.ipojo.annotations.Component;
 import org.apache.felix.ipojo.annotations.Instantiate;
 import org.apache.felix.ipojo.annotations.Property;
 import org.apache.felix.ipojo.annotations.Provides;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
 import org.ow2.chameleon.wisdom.api.Controller;
 import org.ow2.chameleon.wisdom.api.DefaultController;
 import org.ow2.chameleon.wisdom.api.http.HttpMethod;
@@ -13,21 +15,24 @@ import org.ow2.chameleon.wisdom.api.route.Route;
 import org.ow2.chameleon.wisdom.api.route.RouteBuilder;
 
 import java.io.File;
+import java.net.URL;
 import java.util.List;
 
 /**
- * A controller publishing the resources found in a folder.
+ * A controller publishing the resources found in a folder and in bundles
  */
 @Component
 @Provides(specifications = Controller.class)
 @Instantiate(name = "PublicResourceController")
 public class ResourceController extends DefaultController {
 
-    // The default instance handle the `public` folder.
+    // The default instance handle the `assets` folder.
     private final File directory;
+    private final BundleContext context;
 
-    public ResourceController(@Property(value = "public") String path) {
+    public ResourceController(BundleContext bc, @Property(value = "assets") String path) {
         directory = new File(path);
+        this.context = bc;
     }
 
     @Override
@@ -41,12 +46,24 @@ public class ResourceController extends DefaultController {
     public Result serve() {
         File file = new File(directory, context().parameterFromPath("path"));
         if (!file.exists()) {
-            return notFound();
+            return fromBundle(context().parameterFromPath("path"));
         } else {
             return ok(file);
         }
 
         //TODO add ETAG, and Cache configuration.
+    }
+
+    private Result fromBundle(String path) {
+        Bundle[] bundles = context.getBundles();
+        // Skip bundle 0;
+        for (int i = 1; i <= bundles.length; i++) {
+            URL url = bundles[i].getResource("/assets/" + path);
+            if (url != null) {
+                return ok(url);
+            }
+        }
+        return notFound();
     }
 
 
