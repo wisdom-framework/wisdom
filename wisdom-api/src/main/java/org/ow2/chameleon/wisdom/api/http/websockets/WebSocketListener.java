85d70214bafb9f84bc8c1696a401d10a4c3ce4e7
Clement Escoffier <clement.escoffier@gmail.com>
2013-11-16 17:16:27 +0100

    Interfaces related to web socket management
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

---
+++ wisdom-api/src/main/java/org/ow2/chameleon/wisdom/api/http/websockets/WebSocketListener.java
@@ -0,0 +1,15 @@
+package org.ow2.chameleon.wisdom.api.http.websockets;
+
+/**
+ * Classes implementing this interface should register themselves on {@link WebSocketDispatcher} to receive
+ * notification when client are opening, closing web sockets or sending data.
+ */
+public interface WebSocketListener {
+
+    public void received(String uri, byte[] content);
+
+    public void opened(String uri);
+
+    public void closed(String uri);
+
+}
