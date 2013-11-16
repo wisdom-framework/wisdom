91ef2b91b826f13eb6a21791022bcbb57d883ab8
Clement Escoffier <clement.escoffier@gmail.com>
2013-11-16 17:05:29 +0100

    Small refactoring to the equals method of route
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

--- wisdom-api/src/main/java/org/ow2/chameleon/wisdom/api/router/Route.java
+++ wisdom-api/src/main/java/org/ow2/chameleon/wisdom/api/router/Route.java
@@ -167,7 +167,7 @@ public class Route {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null  || ! (o instanceof Route)) return false;
 
         Route route = (Route) o;
 
