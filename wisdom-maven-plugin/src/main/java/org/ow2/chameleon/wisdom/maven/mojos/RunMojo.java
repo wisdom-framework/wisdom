7f4f26afd63763faaf99dad0603dd9243af63c79
Clement Escoffier <clement.escoffier@gmail.com>
2013-11-19 14:06:26 +0100

    Reflect approach change in the run mojo
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

--- wisdom-maven-plugin/src/main/java/org/ow2/chameleon/wisdom/maven/mojos/RunMojo.java
+++ wisdom-maven-plugin/src/main/java/org/ow2/chameleon/wisdom/maven/mojos/RunMojo.java
@@ -1,15 +1,14 @@
 package org.ow2.chameleon.wisdom.maven.mojos;
 
-import org.apache.commons.io.monitor.FileAlterationMonitor;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Execute;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.ResolutionScope;
-import org.ow2.chameleon.wisdom.maven.processors.Pipeline;
-import org.ow2.chameleon.wisdom.maven.processors.Pipelines;
-import org.ow2.chameleon.wisdom.maven.processors.ProcessorException;
+import org.ow2.chameleon.wisdom.maven.WatchingException;
+import org.ow2.chameleon.wisdom.maven.pipeline.Pipeline;
+import org.ow2.chameleon.wisdom.maven.pipeline.Pipelines;
 import org.ow2.chameleon.wisdom.maven.utils.DependencyCopy;
 import org.ow2.chameleon.wisdom.maven.utils.WisdomExecutor;
 import org.ow2.chameleon.wisdom.maven.utils.WisdomRuntimeExpander;
@@ -21,24 +20,27 @@ import java.io.IOException;
  */
 @Mojo(name = "run", threadSafe = false,
         requiresDependencyResolution = ResolutionScope.COMPILE,
-        requiresDirectInvocation = true,
         requiresProject = true
         )
 @Execute(phase = LifecyclePhase.PACKAGE)
 public class RunMojo extends AbstractWisdomMojo {
 
+    private Pipeline pipeline;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         try {
             init();
-        } catch (ProcessorException e) {
+        } catch (WatchingException e) {
             throw new MojoExecutionException(e.getMessage(), e);
         }
 
         new WisdomExecutor().execute(this);
+
+        pipeline.shutdown();
     }
 
-    public void init() throws MojoExecutionException, ProcessorException {
+    public void init() throws MojoExecutionException, WatchingException {
         // Expand if needed.
         if (WisdomRuntimeExpander.expand(this, getWisdomRootDirectory())) {
             getLog().info("Wisdom Runtime installed in " + getWisdomRootDirectory().getAbsolutePath());
@@ -51,8 +53,7 @@ public class RunMojo extends AbstractWisdomMojo {
             throw new MojoExecutionException("Cannot copy dependencies", e);
         }
 
-        Pipelines.fromSession(session)
-                .watch();
+        pipeline = Pipelines.watchers(session, this).watch();
     }
 
 
