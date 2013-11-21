36e962012f5127b609272c47a7bac294302f0a61
Clement Escoffier <clement.escoffier@gmail.com>
2013-11-21 21:33:43 +0100

    Add documentation about session and flash
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

---
+++ documentation/src/main/java/controllers/Documentation.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import org.ow2.chameleon.wisdom.api.DefaultController;
+import org.ow2.chameleon.wisdom.api.annotations.Controller;
+import org.ow2.chameleon.wisdom.api.annotations.Route;
+import org.ow2.chameleon.wisdom.api.http.HttpMethod;
+import org.ow2.chameleon.wisdom.api.http.Result;
+
+@Controller
+public class Documentation extends DefaultController {
+
+    @Route(method = HttpMethod.GET, uri = "/documentation")
+    public Result index() {
+        return redirect("/assets/index.html");
+    }
+}
