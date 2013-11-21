47f636c529fd99bc975bde8f4f72ed756a195352
Clement Escoffier <clement.escoffier@gmail.com>
2013-11-21 21:43:33 +0100

    Add a session controller to support the documentation
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

---
+++ documentation/src/main/java/controllers/Session.java
@@ -0,0 +1,64 @@
+package controllers;
+
+import org.ow2.chameleon.wisdom.api.DefaultController;
+import org.ow2.chameleon.wisdom.api.annotations.Controller;
+import org.ow2.chameleon.wisdom.api.annotations.Route;
+import org.ow2.chameleon.wisdom.api.http.HttpMethod;
+import org.ow2.chameleon.wisdom.api.http.Result;
+
+@Controller
+public class Session extends DefaultController {
+
+    // tag::session[]
+    @Route(method= HttpMethod.GET, uri = "/session")
+    public Result readSession() {
+        String user = session("connected");
+        if(user != null) {
+            return ok("Hello " + user);
+        } else {
+            return unauthorized("Oops, you are not connected");
+        }
+    }
+    // end::session[]
+
+    // tag::login[]
+    @Route(method= HttpMethod.GET, uri = "/session/login")
+    public Result login() {
+        String user = session("connected");
+        if(user != null) {
+            return ok("Already connected");
+        } else {
+            session("connected", "wisdom");
+            return readSession();
+        }
+    }
+    // end::login[]
+
+    // tag::logout[]
+    @Route(method= HttpMethod.GET, uri = "/session/logout")
+    public Result logout() {
+        session().remove("connected");
+        return ok("You have been logged out");
+    }
+    // end::logout[]
+
+    // tag::clear[]
+    @Route(method= HttpMethod.GET, uri = "/session/clear")
+    public Result clear() {
+        session().clear();
+        return ok("You have been logged out");
+    }
+    // end::clear[]
+
+    // tag::flash[]
+    @Route(method= HttpMethod.GET, uri = "/session/flash")
+    public Result welcome() {
+        String message = flash("success");
+        if(message == null) {
+            message = "Welcome!";
+            flash("success", message);
+        }
+        return ok(message);
+    }
+    // end::flash[]
+}
