7250e7dab9bac1a5c1cbd97cc2a19ef371db8089
Clement Escoffier <clement.escoffier@gmail.com>
2013-11-28 21:32:24 +0100

    Expose all interface in the @Controller components
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

--- wisdom-ipojo-module/src/main/java/org/ow2/chameleon/wisdom/ipojo/module/WisdomControllerVisitor.java
+++ wisdom-ipojo-module/src/main/java/org/ow2/chameleon/wisdom/ipojo/module/WisdomControllerVisitor.java
@@ -67,7 +67,7 @@ public class WisdomControllerVisitor extends EmptyVisitor implements AnnotationV
 
     private Element getProvidesElement() {
         Element provides = new Element("provides", "");
-        provides.addAttribute(new Attribute("specifications", Controller.class.getName()));
+        //provides.addAttribute(new Attribute("specifications", Controller.class.getName()));
         return provides;
     }
 
