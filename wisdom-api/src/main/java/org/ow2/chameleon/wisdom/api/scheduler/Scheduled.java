734bae0806358481f1dfdd18f7ceaf5068f684ca
Clement Escoffier <clement.escoffier@gmail.com>
2013-11-29 07:26:18 +0100

    Add the scheduled interface
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

---
+++ wisdom-api/src/main/java/org/ow2/chameleon/wisdom/api/scheduler/Scheduled.java
@@ -0,0 +1,8 @@
+package org.ow2.chameleon.wisdom.api.scheduler;
+
+/**
+ * Marker interface exposed as service by components having periodic jobs.
+ * The job are specified using the {@link org.ow2.chameleon.wisdom.api.annotations.scheduler.Every} annotation.
+ */
+public interface Scheduled {
+}
