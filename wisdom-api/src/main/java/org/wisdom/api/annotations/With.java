0f120bca93349552c26a46ce95ad41f774e5fb00
Clement Escoffier <clement.escoffier@gmail.com>
2013-12-10 16:54:38 +0100

    The With annotation let define an interceptor annotation
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

---
+++ wisdom-api/src/main/java/org/wisdom/api/annotations/With.java
@@ -0,0 +1,19 @@
+package org.wisdom.api.annotations;
+
+import org.wisdom.api.interceptor.Interceptor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Used on annotation configuring an interceptor.
+ * It indicated the interceptor handling the request.
+ */
+@Target(ElementType.ANNOTATION_TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface With {
+
+    Class<? extends Interceptor> value();
+}
