9f7b1aea35f449c44074c93c8515cb8ae08d4984
Clement Escoffier <clement.escoffier@gmail.com>
2013-12-26 14:38:48 +0100

    Fix tests after renaming.
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

--- wisdom-maven-plugin/src/test/java/org/wisdom/maven/utils/DefaultMaven2OSGiConverterTest.java
+++ wisdom-maven-plugin/src/test/java/org/wisdom/maven/utils/DefaultMaven2OSGiConverterTest.java
@@ -16,38 +16,38 @@ public class DefaultMaven2OSGiConverterTest {
         //if artifactId is equal to last section of groupId then groupId is returned. eg.
         // org.apache.maven:maven -> org.apache.maven
         Artifact artifact = new DefaultArtifact("org.acme", "acme", "1.0", null, "jar", "", null);
-        assertThat(DefaultMaven2OSGiConverter.getBundleSymbolicName(artifact)).isEqualTo("org.acme");
+        assertThat(DefaultMaven2OsgiConverter.getBundleSymbolicName(artifact)).isEqualTo("org.acme");
         //if artifactId starts with last section of groupId that portion is removed. eg.
         // org.apache.maven:maven-core -> org.apache.maven.core
         artifact = new DefaultArtifact("org.acme", "acme-api", "1.0", null, "jar", "", null);
-        assertThat(DefaultMaven2OSGiConverter.getBundleSymbolicName(artifact)).isEqualTo("org.acme.api");
+        assertThat(DefaultMaven2OsgiConverter.getBundleSymbolicName(artifact)).isEqualTo("org.acme.api");
         // if artifactId starts with groupId then the artifactId is removed. eg.
         // org.apache:org.apache.maven.core -> org.apache.maven.core
         artifact = new DefaultArtifact("org.acme", "org.acme.api", "1.0", null, "jar", "", null);
-        assertThat(DefaultMaven2OSGiConverter.getBundleSymbolicName(artifact)).isEqualTo("org.acme.api");
+        assertThat(DefaultMaven2OsgiConverter.getBundleSymbolicName(artifact)).isEqualTo("org.acme.api");
         // Default
         artifact = new DefaultArtifact("org.acme", "wisdom-api", "1.0", null, "jar", "", null);
-        assertThat(DefaultMaven2OSGiConverter.getBundleSymbolicName(artifact)).isEqualTo("org.acme.wisdom.api");
+        assertThat(DefaultMaven2OsgiConverter.getBundleSymbolicName(artifact)).isEqualTo("org.acme.wisdom.api");
     }
 
     @Test
     public void testVersionConversion() {
         // Regular versions
-        assertThat(DefaultMaven2OSGiConverter.getVersion("1.0")).isEqualTo("1.0.0");
-        assertThat(DefaultMaven2OSGiConverter.getVersion("1.0.0")).isEqualTo("1.0.0");
-        assertThat(DefaultMaven2OSGiConverter.getVersion("1")).isEqualTo("1.0.0");
+        assertThat(DefaultMaven2OsgiConverter.getVersion("1.0")).isEqualTo("1.0.0");
+        assertThat(DefaultMaven2OsgiConverter.getVersion("1.0.0")).isEqualTo("1.0.0");
+        assertThat(DefaultMaven2OsgiConverter.getVersion("1")).isEqualTo("1.0.0");
 
         // Snapshots
-        assertThat(DefaultMaven2OSGiConverter.getVersion("1.0-SNAPSHOT")).isEqualTo("1.0.0.SNAPSHOT");
-        assertThat(DefaultMaven2OSGiConverter.getVersion("1.0-1.0-20131224.083751-42"))
+        assertThat(DefaultMaven2OsgiConverter.getVersion("1.0-SNAPSHOT")).isEqualTo("1.0.0.SNAPSHOT");
+        assertThat(DefaultMaven2OsgiConverter.getVersion("1.0-1.0-20131224.083751-42"))
                 .isEqualTo("1.0.0.1_0-20131224_083751-42");
 
         // Non-numeric versions
-        assertThat(DefaultMaven2OSGiConverter.getVersion("alpha")).isEqualTo("0.0.0.alpha");
+        assertThat(DefaultMaven2OsgiConverter.getVersion("alpha")).isEqualTo("0.0.0.alpha");
 
         // Version with qualifier
-        assertThat(DefaultMaven2OSGiConverter.getVersion("1.0-alpha")).isEqualTo("1.0.0.alpha");
-        assertThat(DefaultMaven2OSGiConverter.getVersion("1.0.0-RC1")).isEqualTo("1.0.0.RC1");
-        assertThat(DefaultMaven2OSGiConverter.getVersion("1.0-RC-1")).isEqualTo("1.0.0.RC-1");
+        assertThat(DefaultMaven2OsgiConverter.getVersion("1.0-alpha")).isEqualTo("1.0.0.alpha");
+        assertThat(DefaultMaven2OsgiConverter.getVersion("1.0.0-RC1")).isEqualTo("1.0.0.RC1");
+        assertThat(DefaultMaven2OsgiConverter.getVersion("1.0-RC-1")).isEqualTo("1.0.0.RC-1");
     }
 }
