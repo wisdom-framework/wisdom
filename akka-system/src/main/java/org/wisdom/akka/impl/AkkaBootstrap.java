d04764468ec5ce49d07699362ddc71fd60eac29b
Nicolas Rempulski <nrempulski@ubidreams.com>
2014-01-23 16:14:21 +0100

    Refactor AkkaBootstrap to handle multiple Callable types

--- akka-system/src/main/java/org/wisdom/akka/impl/AkkaBootstrap.java
+++ akka-system/src/main/java/org/wisdom/akka/impl/AkkaBootstrap.java
@@ -2,16 +2,20 @@ package org.wisdom.akka.impl;
 
 import akka.actor.ActorSystem;
 import akka.osgi.OsgiActorSystemFactory;
+
 import com.typesafe.config.ConfigFactory;
+
 import org.apache.felix.ipojo.annotations.*;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 import org.wisdom.akka.AkkaSystemService;
 import org.wisdom.api.http.Context;
 import org.wisdom.api.http.Result;
+
 import scala.concurrent.ExecutionContext;
 import scala.concurrent.Future;
 
+import java.io.InputStream;
 import java.util.concurrent.Callable;
 
 @Component
@@ -56,13 +60,21 @@ public class AkkaBootstrap implements AkkaSystemService {
     }
 
     @Override
-    public Future<Result> dispatch(Callable<Result> callable, Context context) {
+    public Future<Result> dispatchResultWithContext(Callable<Result> callable, Context context) {
         return akka.dispatch.Futures.future(callable,
                 new HttpExecutionContext(system.dispatcher(), context, Thread.currentThread().getContextClassLoader()));
     }
 
     @Override
-    public Future<Result> dispatch(Callable<Result> callable) {
+    public Future<Result> dispatchResult(Callable<Result> callable) {
+        return akka.dispatch.Futures.future(callable,
+                new HttpExecutionContext(system.dispatcher(), Context.context.get(),
+                        Thread.currentThread().getContextClassLoader
+                                ()));
+    }
+    
+    @Override
+    public Future<InputStream> dispatchInputStream(Callable<InputStream> callable) {
         return akka.dispatch.Futures.future(callable,
                 new HttpExecutionContext(system.dispatcher(), Context.context.get(),
                         Thread.currentThread().getContextClassLoader
