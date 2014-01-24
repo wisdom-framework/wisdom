15111edaa8404a63a0ee134d77970abf53c4adae
Nicolas Rempulski <nrempulski@ubidreams.com>
2014-01-24 11:14:15 +0100

    [TD] major - minors
    * Replace all tab characters in this file by sequences of white-spaces.
    * Move this variable to comply with Java Code Conventions.
    * Replace this usage of System.out or System.err by a logger.

--- wisdom-test/src/main/java/org/wisdom/test/internals/ProbeBundleMaker.java
+++ wisdom-test/src/main/java/org/wisdom/test/internals/ProbeBundleMaker.java
@@ -4,9 +4,14 @@ import aQute.bnd.osgi.Builder;
 import aQute.bnd.osgi.Constants;
 import aQute.bnd.osgi.Jar;
 import aQute.bnd.osgi.Processor;
+
 import com.google.common.reflect.ClassPath;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.felix.ipojo.manipulator.Pojoization;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.wisdom.test.WisdomBlackBoxRunner;
 import org.wisdom.test.probe.Activator;
 
 import java.io.File;
@@ -28,6 +33,10 @@ public class ProbeBundleMaker {
     public static final String PACKAGES_TO_ADD = "org.wisdom.test.parents.*, " +
             "org.wisdom.test.probe";
     public static final String PROBE_FILE = "target/osgi/probe.jar";
+    
+    public static final String TEST_CLASSES = "target/test-classes";
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProbeBundleMaker.class);
 
     static {
         // At initialization, delete the probe bundle if exist
@@ -36,12 +45,10 @@ public class ProbeBundleMaker {
             FileUtils.deleteQuietly(probe);
         }
     }
-    
-    private ProbeBundleMaker(){
-    	//Unused
-    }
 
-    public static final String TEST_CLASSES = "target/test-classes";
+    private ProbeBundleMaker(){
+        //Unused
+    }
 
     public static InputStream probe() throws Exception {
         File probe = new File(PROBE_FILE);
@@ -127,7 +134,7 @@ public class ProbeBundleMaker {
     }
 
     protected static Builder getOSGiBuilder(Properties properties,
-                                            Jar[] classpath) throws Exception {
+            Jar[] classpath) throws Exception {
         Builder builder = new Builder();
         // protect setBase...getBndLastModified which uses static DateFormat
         synchronized (ProbeBundleMaker.class) {
@@ -186,7 +193,7 @@ public class ProbeBundleMaker {
 
     protected static boolean reportErrors(String prefix, List<String> warnings, List<String> errors) {
         for (String msg : warnings) {
-            System.err.println(prefix + " : " + msg);
+            LOGGER.error(prefix + " : " + msg);
         }
 
         boolean hasErrors = false;
