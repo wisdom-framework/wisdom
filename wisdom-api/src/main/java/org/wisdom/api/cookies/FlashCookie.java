acecec5a19c2ca3ac8752eafa36b1dc3a3904a01
Clement Escoffier <clement.escoffier@gmail.com>
2014-03-24 20:50:23 +0100

    Added javadoc to the flash cookie class
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

--- wisdom-api/src/main/java/org/wisdom/api/cookies/FlashCookie.java
+++ wisdom-api/src/main/java/org/wisdom/api/cookies/FlashCookie.java
@@ -12,15 +12,37 @@ import java.util.Map;
  * Stuff in a flash cookie gets deleted after the next request.
  * 
  * Please note also that flash cookies are not signed.
+ *
+ * This class uses 'current' for the retrieve flash values and 'outgoing' for the data write back.
  */
 public interface FlashCookie {
 
+    /**
+     * Initializes the cookie.
+     * It reads the content of the flash cookie (current values).
+     * @param context the context
+     */
     void init(Context context);
 
+    /**
+     * Writes the flash content to the cookie (outgoing values).
+     * @param context the context
+     * @param result the result
+     */
     void save(Context context, Result result);
 
+    /**
+     * Sets the given key to the given value.
+     * @param key the key
+     * @param value the value
+     */
     void put(String key, String value);
 
+    /**
+     * Sets the given key to the given value.
+     * @param key the key
+     * @param value the value
+     */
     void put(String key, Object value);
 
     /**
@@ -41,25 +63,63 @@ public interface FlashCookie {
      */
     void success(String value);
 
+    /**
+     * Removes the given key from the outgoing data.
+     * @param key the key
+     */
     void discard(String key);
 
+    /**
+     * Clears the outgoing values.
+     */
     void discard();
 
+    /**
+     * Transfers the given key/value to the next response.
+     * @param key the key
+     */
     void keep(String key);
 
+    /**
+     * Transfers the whole flash cookie to the next response.
+     */
     void keep();
 
+    /**
+     * Gets the current value.
+     * @param key the key
+     * @return the current value, {@literal null} if not set
+     */
     String get(String key);
 
+    /**
+     * Removes the given key of the current data.
+     * @param key the key
+     * @return {@literal true} if the entry was removed, {@literal false} otherwise
+     */
     boolean remove(String key);
 
+    /**
+     * Clear the current data.
+     */
     void clearCurrentFlashCookieData();
 
+    /**
+     * Checks whether the current data contains the given key.
+     * @param key the key
+     * @return {@literal true} if the current data contains the given key, {@literal false} otherwise
+     */
     boolean contains(String key);
 
-    String toString();
-
+    /**
+     * Gets the current data.
+     * @return the current data
+     */
     Map<String, String> getCurrentFlashCookieData();
 
+    /**
+     * Gets the outgoing data.
+     * @return the outgoing data
+     */
     Map<String, String> getOutgoingFlashCookieData();
 }
