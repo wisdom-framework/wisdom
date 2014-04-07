c524342c1513ea8759861a4d1f964958fe32b91a
Jonathan Bardin <jbardin@tech-arts.com>
2014-04-07 13:35:19 +0700

    Add licence, and terminal tab.

--- extensions/wisdom-monitor/src/main/java/org/wisdom/monitor/MonitorController.java
+++ extensions/wisdom-monitor/src/main/java/org/wisdom/monitor/MonitorController.java
@@ -1,3 +1,22 @@
+/*
+ * #%L
+ * Wisdom-Framework
+ * %%
+ * Copyright (C) 2013 - 2014 Wisdom Framework
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package org.wisdom.monitor;
 
 import akka.actor.Cancellable;
@@ -62,6 +81,9 @@ public class MonitorController extends DefaultController {
 
     @View("bundles")
     Template bundles;
+
+    @View("terminal")
+    Template terminal;
 
     @Context
     BundleContext context;
@@ -129,6 +151,11 @@ public class MonitorController extends DefaultController {
     @Route(method = HttpMethod.GET, uri = "/bundles")
     public Result bundle() {
         return ok(render(bundles));
+    }
+
+    @Route(method = HttpMethod.GET, uri = "/terminal")
+    public Result terminal() {
+        return ok(render(terminal));
     }
 
     private int getProviderBundleCount(List<ServiceModel> svc) {
