9b696b7418e797dddbda1bd07e5dbdca255d5eb9
Clement Escoffier <clement.escoffier@gmail.com>
2014-05-14 18:10:16 +0200

    Fix #208
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

--- core/wisdom-maven-plugin/src/main/java/org/wisdom/maven/Constants.java
+++ core/wisdom-maven-plugin/src/main/java/org/wisdom/maven/Constants.java
@@ -42,8 +42,8 @@ public interface Constants {
     public static final String TEST_SRC_DIR = "src/test/java";
     public static final String TEST_RESOURCES_DIR = "src/test/resources";
 
-    public static final String NODE_VERSION = "0.10.26";
-    public static final String NPM_VERSION = "1.4.6";
+    public static final String NODE_VERSION = "0.10.28";
+    public static final String NPM_VERSION = "1.4.10";
 
     public static final String INSTRUCTIONS_FILE = "src/main/osgi/osgi.bnd";
 
