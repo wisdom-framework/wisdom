e68a643f13f1a25e0375ec092c3713db957c42f7
Clement Escoffier <clement.escoffier@gmail.com>
2014-06-25 14:52:32 +0200

    Fix #238
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

---
+++ core/application-configuration/src/main/java/org/wisdom/configuration/ConfigurationAdminBackend.java
@@ -0,0 +1,71 @@
+/*
+ * #%L
+ * Wisdom-Framework
+ * %%
+ * Copyright (C) 2013 - 2014 Wisdom Framework
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.wisdom.configuration;
+
+import org.apache.felix.cm.PersistenceManager;
+import org.apache.felix.cm.file.FilePersistenceManager;
+import org.apache.felix.ipojo.annotations.*;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.wisdom.api.configuration.ApplicationConfiguration;
+
+/**
+ * This component provides the Persistence Manager used by Felix Implementation of the Configuration Admin. It set the
+ * location where the configurations are stored. If not configured, the configurations are stored into {@literal
+ * basedir/conf/configurations}
+ */
+@Component
+@Instantiate
+public class ConfigurationAdminBackend {
+
+    /**
+     * The location where configurations will be stored.
+     */
+    public static final String LOCATION = "conf/configurations";
+
+    /**
+     * The service registration, marked as protected for testing purpose.
+     */
+    protected ServiceRegistration<PersistenceManager> reg;
+
+    public ConfigurationAdminBackend(@Context BundleContext context,
+                                     @Requires ApplicationConfiguration configuration) {
+        if (!configuration.getBooleanWithDefault("configadmin.persistence", true)) {
+            return;
+        }
+
+        String loc = configuration.getWithDefault("configadmin.storage", LOCATION);
+        FilePersistenceManager delegate = new FilePersistenceManager(loc);
+        reg = context.registerService(PersistenceManager.class, delegate, null);
+    }
+
+    @Invalidate
+    public void unregisterQuietly() {
+        if (reg == null) {
+            return;
+        }
+        try {
+            reg.unregister();
+        } catch (IllegalStateException e) { //NOSONAR
+            // Swallow the exception
+        }
+        reg = null;
+    }
+}
---
+++ core/application-configuration/src/test/java/org/wisdom/configuration/ConfigurationAdminBackendTest.java
@@ -0,0 +1,70 @@
+/*
+ * #%L
+ * Wisdom-Framework
+ * %%
+ * Copyright (C) 2013 - 2014 Wisdom Framework
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.wisdom.configuration;
+
+import org.apache.felix.cm.PersistenceManager;
+import org.junit.Test;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.wisdom.api.configuration.ApplicationConfiguration;
+
+import java.util.Dictionary;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ConfigurationAdminBackendTest {
+
+    @Test
+    public void backendEnabledWithDefaultLocation() {
+        BundleContext context = mock(BundleContext.class);
+        ServiceRegistration<PersistenceManager> registration = mock(ServiceRegistration.class);
+        when(context.registerService(any(Class.class), any(PersistenceManager.class),
+                any(Dictionary.class))).thenReturn(registration);
+        ApplicationConfiguration configuration = mock(ApplicationConfiguration.class);
+        when(configuration.getBooleanWithDefault("configadmin.persistence", true)).thenReturn(true);
+        when(configuration.getWithDefault("configadmin.storage", ConfigurationAdminBackend.LOCATION)).thenReturn
+                (ConfigurationAdminBackend.LOCATION);
+
+        ConfigurationAdminBackend backend = new ConfigurationAdminBackend(context, configuration);
+        assertThat(backend.reg).isNotNull();
+        assertThat(backend.reg).isEqualTo(registration);
+        backend.unregisterQuietly();
+    }
+
+    @Test
+    public void backendDisabledWithDefaultLocation() {
+        BundleContext context = mock(BundleContext.class);
+        ServiceRegistration<PersistenceManager> registration = mock(ServiceRegistration.class);
+        when(context.registerService(any(Class.class), any(PersistenceManager.class),
+                any(Dictionary.class))).thenReturn(registration);
+        ApplicationConfiguration configuration = mock(ApplicationConfiguration.class);
+        when(configuration.getBooleanWithDefault("configadmin.persistence", true)).thenReturn(false);
+        when(configuration.getWithDefault("configadmin.storage", ConfigurationAdminBackend.LOCATION)).thenReturn
+                (ConfigurationAdminBackend.LOCATION);
+
+        ConfigurationAdminBackend backend = new ConfigurationAdminBackend(context, configuration);
+        assertThat(backend.reg).isNull();
+        backend.unregisterQuietly();
+    }
+
+}
