c0c3d0bd5350e27e92ca15dcaeb0ca4a8a5c5f72
Clement Escoffier <clement.escoffier@gmail.com>
2014-07-04 08:49:55 +0200

    Fix test
    Add edited the test for the documentation... forgot to revert my changes.
    
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

--- framework/integration-tests/src/main/java/controllers/errors/NoSuchElementExceptionMapper.java
+++ framework/integration-tests/src/main/java/controllers/errors/NoSuchElementExceptionMapper.java
@@ -48,6 +48,6 @@ public class NoSuchElementExceptionMapper implements ExceptionMapper<NoSuchEleme
      */
     @Override
     public Result toResult(NoSuchElementException exception) {
-        return new Result().status(404).render("nobody there");
+        return new Result().status(300).render("nobody");
     }
 }
