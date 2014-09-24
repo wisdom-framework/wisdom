512b8eeb70537799be6729768003b8aff67bd3a5
barjo <astrosus@gmail.com>
2014-09-24 19:27:41 +0700

    add getTransactionManager in Crud

--- core/wisdom-api/src/main/java/org/wisdom/api/model/Crud.java
+++ core/wisdom-api/src/main/java/org/wisdom/api/model/Crud.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -177,6 +177,11 @@ public interface Crud<T, I extends Serializable> {
      * @throws RollBackHasCauseAnException if an exception occurred when the transaction is rollback.
      */
     <A> A executeTransactionalBlock(Callable<A> callable) throws HasBeenRollBackException;
+
+    /**
+     * @return The {@link TransactionManager} used by this crud in order to run the transaction.
+     */
+    TransactionManager getTransactionManager();
 
     /**
      * Create a FluentTransaction with this Crud service,
