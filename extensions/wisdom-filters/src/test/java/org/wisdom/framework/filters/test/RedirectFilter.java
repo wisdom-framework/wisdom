334e097a5c3402627f79267caad206673a1e69f6
Clement Escoffier <clement.escoffier@gmail.com>
2014-11-10 19:26:49 +0100

    Cleanup code
    Add missing headers
    Remove links to static files
    
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

--- extensions/wisdom-filters/src/test/java/org/wisdom/framework/filters/test/RedirectFilter.java
+++ extensions/wisdom-filters/src/test/java/org/wisdom/framework/filters/test/RedirectFilter.java
@@ -1,28 +1,32 @@
+/*
+ * #%L
+ * Wisdom-Framework
+ * %%
+ * Copyright (C) 2013 - 2014 Wisdom Framework
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package org.wisdom.framework.filters.test;
 
-import org.apache.felix.ipojo.annotations.Validate;
 import org.wisdom.api.annotations.Service;
-import org.wisdom.api.http.HttpMethod;
-import org.wisdom.api.http.Request;
 import org.wisdom.api.interception.Filter;
 import org.wisdom.framework.filters.ProxyFilter;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.logging.LogManager;
 import java.util.regex.Pattern;
 
 @Service
 public class RedirectFilter extends ProxyFilter implements Filter {
-
-    @Validate
-    public void init() throws IOException {
-        LogManager.getLogManager().readConfiguration(new FileInputStream("/Users/clement/Projects/wisdom/wisdom-filters/src/logging.properties"));
-        System.out.println("Configured");
-    }
 
     @Override
     protected String getProxyTo() {
--- extensions/wisdom-filters/src/test/java/org/wisdom/framework/filters/test/XMLFilter.java
+++ extensions/wisdom-filters/src/test/java/org/wisdom/framework/filters/test/XMLFilter.java
@@ -1,9 +1,27 @@
+/*
+ * #%L
+ * Wisdom-Framework
+ * %%
+ * Copyright (C) 2013 - 2014 Wisdom Framework
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package org.wisdom.framework.filters.test;
 
 import org.apache.felix.ipojo.annotations.Component;
 import org.apache.felix.ipojo.annotations.Instantiate;
 import org.apache.felix.ipojo.annotations.Provides;
-import org.apache.felix.ipojo.annotations.Validate;
 import org.wisdom.api.interception.Filter;
 import org.wisdom.framework.filters.ProxyFilter;
 
@@ -13,11 +31,6 @@ import java.util.regex.Pattern;
 @Provides(specifications = Filter.class)
 @Instantiate
 public class XMLFilter extends ProxyFilter implements Filter {
-
-    @Validate
-    public void init() {
-        System.out.println("Ready...");
-    }
 
     @Override
     protected String getProxyTo() {
